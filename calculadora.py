d7d6374a633e3bbd5a09f5131e69666dfc49033a
Marcos Pedro Camargo <mcmpgui12@gmail.com>
2025-02-26 01:54:58 -0300

    Terminei a o coversor de moeda
    1. Definindo a chave da API e URL base
    2. Função de conversão de moeda
    3. Solicitação da API
    4. Verificação do status da API
    5. Extração dos dados de câmbio
    6. Obtenção da taxa de câmbio das moedas
    7. Verificação de moedas inválidas
    8. Realização da conversão
    9. Captura de dados do usuário
    10. Tratamento de erro para o valor
    11. Exibição do resultado
    11.1 Exibe o resultado da conversão formatado com duas casas decimais.

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,45 @@
-n1 = 20
-n2 = 45
-total = n1 + n2
-print(f'O total da soma é {total}')
+    import requests
+
+    API_KEY = "005a8e405a0f4eba80671e4c1715b306"
+    BASE_URL = f"https://api.currencyfreaks.com/latest?apikey={API_KEY}"
+
+    def converter_moeda(moeda_origem, moeda_destino, valor):
+        resposta = requests.get(BASE_URL)
+        
+        if resposta.status_code != 200:
+            print("Erro ao obter dados da API. Verifique sua chave de API.")
+            return None
+
+        dados = resposta.json()
+        taxas = dados.get("rates", {})
+
+    # Obtem a taxa de cambio das moedas
+        taxa_origem = float(taxas.get(moeda_origem, 0))
+        taxa_destino = float(taxas.get(moeda_destino, 0))
+
+    # Verifica as moedas fornecidas se ainda são validas
+        if taxa_origem == 0 or taxa_destino == 0:
+            print("Moeda inválida ou não suportada pela API.")
+            return None
+
+    # Realiza a conversão do valor 
+        valor_convertido = (valor / taxa_origem) * taxa_destino
+        return valor_convertido
+
+    # Captura os dados de entrada do usuario 
+    moeda_origem = input("Digite a moeda de origem : ").upper() # Precisa saber as siglas das moedas 
+    moeda_destino = input("Digite a moeda de destino : ").upper() 
+
+    try:
+        valor = float(input("Digite o valor a ser convertido: "))
+    except ValueError:
+        print("Erro: Digite um número válido.")
+        exit()
+
+    resultado = converter_moeda(moeda_origem, moeda_destino, valor)
+
+    # Chama a função de conversão e exibe o resultado
+    if resultado is not None:
+        print(f"{valor:.2f} {moeda_origem} equivale a {resultado:.2f} {moeda_destino}")
+
+
